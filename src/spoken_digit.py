0794f3ca61c5776ca25e488be039d3552540f050
Mohsin Baig <moebagels@gmail.com>
2018-07-26 17:13:18 -0500

    model built and trained

--- src/spoken_digit.py
+++ src/spoken_digit.py
@@ -27,8 +27,43 @@ def extract_mfcc(file_path, utterance_length):
     return mfcc_features
 
 #
-# GET MFCC BATCH
+# GET TRAINING BATCH, returns data in batches 
 #
+def get_mfcc_batch(file_path, batch_size, utterance_length):
+    print("hello")
+    files = os.listdir(file_path)
+    ft_batch = []
+    label_batch = []
+
+    while True:
+        # Shuffle Files
+        random.shuffle(files)
+        for fname in files:
+            # print("Total %d files in directory" % len(files))
+
+            # Make sure file is a .wav file
+            if not fname.endswith(".wav"):
+                continue
+            
+            # Get MFCC Features for the file
+            mfcc_features = extract_mfcc(file_path + fname, utterance_length)
+            
+            # One-hot encode label for 10 digits 0-9
+            label = np.eye(10)[int(fname[0])]
+            
+            # Append to label batch
+            label_batch.append(label)
+            
+            # Append mfcc features to ft_batch
+            ft_batch.append(mfcc_features)
+
+            # Check to see if default batch size is < than ft_batch
+            if len(ft_batch) >= batch_size:
+                # send over batch
+                yield ft_batch, label_batch
+                # reset batches
+                ft_batch = []
+                labels_batch = []
 
 #
 # DISPLAY FEATURE SHAPE
@@ -54,27 +89,39 @@ def display_power_spectrum(wav_file_path, utterance_length):
 def main():
     # Initial Parameters
     lr = 0.001
-    iterations_train = 30
+    iterations_train = 50
     bsize = 64
     audio_features = 20  
     utterance_length = 35  # Modify to see what different results you can get
     ndigits = 10
 
-    # Test
-    test_file = '../data/recordings/train/0_jackson_13.wav'
-    display_power_spectrum(test_file, utterance_length)
-
-    # Preprocess Data
-
-    # Build Model
+    # Get training data
+    train_batch = get_mfcc_batch('../data/recordings/train/', 256, utterance_length)
+    
+    # # Build Model
+    sp_network = tflearn.input_data([None, audio_features, utterance_length])
+    sp_network = tflearn.lstm(sp_network, 128*4, dropout=0.5)
+    sp_network = tflearn.fully_connected(sp_network, ndigits, activation='softmax')
+    sp_network = tflearn.regression(sp_network, optimizer='adam', learning_rate=lr, loss='categorical_crossentropy')
+    sp_model = tflearn.DNN(sp_network, tensorboard_verbose=0)
 
     # Train Model
+    while iterations_train > 0:
+        X_tr, y_tr = next(train_batch)
+        X_test, y_test = next(train_batch)
+        sp_model.fit(X_tr, y_tr, n_epoch=10, validation_set=(X_test, y_test), show_metric=True, batch_size=bsize)
+        iterations_train -=1
+    sp_model.save("../model/speech_recognition.lstm")
 
     # Test Model
-    
-     
+    sp_model.load('../model/speech_recognition.lstm')
+    mfcc_features = extract_mfcc('../data/recordings/test/3_jackson_49.wav', utterance_length)
+    mfcc_features = mfcc_features.reshape((1,mfcc_features.shape[0],mfcc_features.shape[1]))
+    prediction_digit = sp_model.predict(mfcc_features)
+    print(prediction_digit)
+    print("Digit predicted: ", np.argmax(prediction_digit))
+
     # Done
-    print("DONE")
     return 0
 
 
